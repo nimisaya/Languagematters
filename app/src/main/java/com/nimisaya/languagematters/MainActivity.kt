4b9944f1bc6676f4b63267306e2a268d3c3dcc86
Amanda Jarvinen <nimisaya@outlook.com>
2023-02-17 18:46:28 +1100

    Add greeting in English, Indonesian and Australian English

--- app/src/main/java/com/nimisaya/languagematters/MainActivity.kt
+++ app/src/main/java/com/nimisaya/languagematters/MainActivity.kt
@@ -3,13 +3,15 @@ package com.nimisaya.languagematters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import com.nimisaya.languagematters.annotations.LanguagePreviews
 import com.nimisaya.languagematters.ui.theme.LanguageMattersTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +19,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LanguageMattersTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
@@ -31,10 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = stringResource(id = R.string.hello) + name)
+        Text(text = stringResource(id = R.string.question_how_are_you))
+    }
 }
 
-@Preview(showBackground = true)
+@LanguagePreviews
 @Composable
 fun DefaultPreview() {
     LanguageMattersTheme {
---
+++ app/src/main/java/com/nimisaya/languagematters/annotations/PreviewAnnotations.kt
@@ -0,0 +1,8 @@
+package com.nimisaya.languagematters.annotations
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Preview(locale = "in")
+@Preview(locale = "en-rAU")
+annotation class LanguagePreviews
